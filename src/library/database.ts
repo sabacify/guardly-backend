b4e4d2dd8c420114f3dea6b327b7e3f8c7289b31
Aquib Momin <sabacify@gmail.com>
2023-11-03 06:08:31 +0800

    ssl true

--- src/library/database.ts
+++ src/library/database.ts
@@ -17,6 +17,7 @@ export class Database {
         user: 'postgres', // 'guardly_rwuser'
         password: 'SCBpassword1$', // 'admin'
         database: 'guardly',
+        ssl: true
       },
       searchPath: ['app']
     });
